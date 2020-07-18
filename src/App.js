bb9daf8dfaf985effbeb5a28543edc9976315846
renatalourena <renatalourena@gmail.com>
2020-07-18 15:24:22 -0300

    remove unused dependency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import './App.css';
 import Login from './components/Login'
 
